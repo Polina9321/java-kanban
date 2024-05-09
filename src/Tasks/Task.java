25ce0cd9c1982491568b680cf2da50b01dd2c0d3
Polina <polinakiriak1@gmail.com>
2024-05-09 12:21:47 +0300

    Реализовала логику работы класса для простых задач

---
+++ src/Tasks/Task.java
@@ -0,0 +1,61 @@
+package Tasks;
+
+import java.util.Objects;
+
+public class Task {
+    protected String nameTask;
+    protected String contentTask;
+    protected final int idTask;
+    protected Status statusTask;
+
+    public Task(String nameTask, String contentTask) {
+        this.nameTask = nameTask;
+        this.contentTask = contentTask;
+        idTask = hashCode();
+        statusTask = Status.NEW;
+    }
+
+    public void setStatusTask(Status statusTask) {
+        if (statusTask == Status.DONE || statusTask == Status.IN_PROGRESS || statusTask == Status.NEW) {
+            System.out.println("Статус не найден.");
+        } else {
+            this.statusTask = statusTask;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "ID - " + idTask;
+        if (nameTask == null) { result += ""; }
+        else { result += "\nНазвание - " + nameTask; }
+        if (contentTask == null) { result += ""; }
+        else { result += "\nСодержание - " + contentTask; }
+        result += "\nСтатус - " + statusTask;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return idTask == task.idTask && Objects.equals(nameTask, task.nameTask) && Objects.equals(contentTask, task.contentTask) && statusTask == task.statusTask;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameTask, contentTask, idTask, statusTask);
+    }
+
+    public Status getStatusTask() {
+        return statusTask;
+    }
+
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
+
+    public void setContentTask(String contentTask) {
+        this.contentTask = contentTask;
+    }
+}
