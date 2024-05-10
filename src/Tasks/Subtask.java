8bc6be2415d91d39cd5185782bc63671f37d3800
Polina <polinakiriak1@gmail.com>
2024-05-10 12:55:44 +0300

    Добавила метод получения id подзадачи

--- src/Tasks/Subtask.java
+++ src/Tasks/Subtask.java
@@ -19,6 +19,10 @@ public class Subtask extends Task {
         return result;
     }
 
+    public int getSubtaskId() {
+        return subtaskId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
